adf37ea5e6b023ce1eab38a0e104b6fe0523d27f
Rushikesh K <rpkrushikumar@gmail.com>
2023-02-02 16:50:59 +0000

    Editing description for commands

--- index.js
+++ index.js
@@ -11,25 +11,25 @@ const path = require("path")
 commander.program
         .command('CREATE <ACCNO> <ACCNAME>')
         .alias('create')
-        .description('This command used to create account in kelp-bank')
+        .description('This command used to CREATE an account in kelp-bank')
         .action(create)
 
 commander.program
         .command('DEPOSIT <accNo> <amount>')
         .alias('deposit')
-        .description('This command used to create account in kelp-bank')
+        .description('This command used to DEPOSIT amount to kelp-bank account')
         .action(deposite)
 
 commander.program
         .command('WITHDRAW <accNo> <amount>')
         .alias('withdraw')
-        .description('This command used to create account in kelp-bank')
+        .description('This command used to WITHDRAW amount from kelp-bank account')
         .action(withdraw)
 
 commander.program
         .command('BALANCE <accNo> [accName]')
         .alias('balance')
-        .description('This command used to create account in kelp-bank')
+        .description('This command used to check BALANCE in kelp-bank account')
         .action(show)
 
 commander.program.parse()
